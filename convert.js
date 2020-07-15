440d7c4aba3b64ee4477d0d0bff907f6050db48b
blanck <jonas@blanck.hk>
2020-07-15 10:24:14 +0200

    fix lint

--- convert.js
+++ convert.js
@@ -1,27 +1,29 @@
-var converter = require('api-spec-converter');
-var fs = require('fs');
+const converter = require('api-spec-converter');
+const fs = require('fs');
 // Convert to openapi 2 / swagger
 converter.convert({
   from: 'openapi_3',
   to: 'swagger_2',
   source: 'swagger.yaml',
 })
-  .then(function(converted) {
+    .then(function(converted) {
     // [Optional] Fill missing fields with dummy values
-    converted.fillMissing();
+      converted.fillMissing();
 
-    // [Optional] Validate converted spec
-    return converted.validate()
-      .then(function (result) {
-        if (result.errors)
-          // return console.error(JSON.stringify(result.errors, null, 2));
-      	  console.log(JSON.stringify(result.errors, null, 2));
-        if (result.warnings)
-          return console.error(JSON.stringify(result.warnings, null, 2));
+      // [Optional] Validate converted spec
+      return converted.validate()
+          .then(function(result) {
+            if (result.errors) {
+              // return console.error(JSON.stringify(result.errors, null, 2));
+              console.log(JSON.stringify(result.errors, null, 2));
+            }
+            if (result.warnings) {
+              return console.error(JSON.stringify(result.warnings, null, 2));
+            }
 
-        fs.writeFileSync('swagger2.json', converted.stringify());
-      });
-  });
+            fs.writeFileSync('swagger2.json', converted.stringify());
+          });
+    });
 
 // Convert to connector
 converter.convert({
@@ -29,22 +31,26 @@ converter.convert({
   to: 'swagger_2',
   source: 'swagger.yaml',
 })
-  .then(function(converted) {
+    .then(function(converted) {
     // [Optional] Fill missing fields with dummy values
-    converted.fillMissing();
+      converted.fillMissing();
 
-    // [Optional] Validate converted spec
-    return converted.validate()
-      .then(function (result) {
-        if (result.errors)
-          // return console.error(JSON.stringify(result.errors, null, 2));
-      	  console.log(JSON.stringify(result.errors, null, 2));
-        if (result.warnings)
-          return console.error(JSON.stringify(result.warnings, null, 2));
-
-        fs.writeFileSync('connector/apiDefinition.swagger.json', converted.stringify());
-      });
-  });
+      // [Optional] Validate converted spec
+      return converted.validate()
+          .then(function(result) {
+            if (result.errors) {
+              // return console.error(JSON.stringify(result.errors, null, 2));
+              console.log(JSON.stringify(result.errors, null, 2));
+            }
+            if (result.warnings) {
+              return console.error(JSON.stringify(result.warnings, null, 2));
+            }
+            fs.writeFileSync(
+                'connector/apiDefinition.swagger.json',
+                converted.stringify(),
+            );
+          });
+    });
 
 // Convert to openapi 2 yaml
 converter.convert({
@@ -52,22 +58,24 @@ converter.convert({
   to: 'swagger_2',
   source: 'swagger.yaml',
 })
-  .then(function(converted) {
+    .then(function(converted) {
     // [Optional] Fill missing fields with dummy values
-    converted.fillMissing();
+      converted.fillMissing();
 
-    // [Optional] Validate converted spec
-    return converted.validate()
-      .then(function (result) {
-        if (result.errors)
-          // return console.error(JSON.stringify(result.errors, null, 2));
-      	  console.log(JSON.stringify(result.errors, null, 2));
-        if (result.warnings)
-          return console.error(JSON.stringify(result.warnings, null, 2));
-        var options = {syntax: 'yaml', order: 'openapi'};
-        fs.writeFileSync('swagger2.yaml', converted.stringify(options));
-      });
-  });
+      // [Optional] Validate converted spec
+      return converted.validate()
+          .then(function(result) {
+            if (result.errors) {
+              // return console.error(JSON.stringify(result.errors, null, 2));
+              console.log(JSON.stringify(result.errors, null, 2));
+            }
+            if (result.warnings) {
+              return console.error(JSON.stringify(result.warnings, null, 2));
+            }
+            const options = {syntax: 'yaml', order: 'openapi'};
+            fs.writeFileSync('swagger2.yaml', converted.stringify(options));
+          });
+    });
 
 // Convert to openapi 3 json
 converter.convert({
@@ -75,19 +83,21 @@ converter.convert({
   to: 'openapi_3',
   source: 'swagger.yaml',
 })
-  .then(function(converted) {
+    .then(function(converted) {
     // [Optional] Fill missing fields with dummy values
-    converted.fillMissing();
+      converted.fillMissing();
 
-    // [Optional] Validate converted spec
-    return converted.validate()
-      .then(function (result) {
-        if (result.errors)
-          // return console.error(JSON.stringify(result.errors, null, 2));
-      	  console.log(JSON.stringify(result.errors, null, 2));
-        if (result.warnings)
-          return console.error(JSON.stringify(result.warnings, null, 2));
+      // [Optional] Validate converted spec
+      return converted.validate()
+          .then(function(result) {
+            if (result.errors) {
+              // return console.error(JSON.stringify(result.errors, null, 2));
+              console.log(JSON.stringify(result.errors, null, 2));
+            }
+            if (result.warnings) {
+              return console.error(JSON.stringify(result.warnings, null, 2));
+            }
 
-        fs.writeFileSync('openapi.json', converted.stringify());
-      });
-  });
+            fs.writeFileSync('openapi.json', converted.stringify());
+          });
+    });
